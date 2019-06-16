10e9021b0138aa2b40dd2355096bbb5cf65fc40d
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-06-16 09:57:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,7 +28,27 @@ function ternaryTeenager(age) {if (age >= 13 && age <= 19) {
     return "You are not a teenager"
 }
 
-
-function switchAge(age) {
-
+function switchAge(age) {case "13":
+    return "You are a teenager";
+    break;
+  case "14":
+    return "You are a teenager";
+    break;
+  case "15":
+    return "You are a teenager";
+    break;
+  case "16":
+    return "You are a teenager";
+    break;
+    case "17":
+    return "You are a teenager";
+    break;
+    case "18":
+    return "You are a teenager";
+    break;
+    case "19":
+    return "You are a teenager";
+    break;
+  default:
+    return "You have an age";
 }
